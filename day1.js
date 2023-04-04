9551e01326087a09965fd4bc9268d5017899691f
aulrizky <auliarr@gmail.com>
2023-04-04 16:30:26 +0700

    yeh

--- day1.js
+++ day1.js
@@ -1,2 +1,29 @@
-var num = 5;
-alert(hi)
+// day 1 
+// var ten = 10
+// console.log(ten * ten)17777414865132
+
+// var star = ""
+// for (var i = 0; i < 10; i++) {
+//     for (var j = 0; j < i; j++) {
+//         star += "*"
+//     }
+//     star += "\n"
+// }
+
+// console.log(star)
+// for (var i = 0; i <15;i++) {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     console.log("fizzbuzz")
+//     else if (i %5==0)
+//     console.log("buxx")
+//     else if (i % 3 == 0) 
+//     console.log("fizz")
+// }
+
+// console.log(i)
+
+var square = function(x) {
+    return x * x;
+};
+
+console.log(square(10));
